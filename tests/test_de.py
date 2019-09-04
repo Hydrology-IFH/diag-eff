af5f43e3a293ae016ecce650e711b91d83fc17d1
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2019-09-04 14:25:22 +0200

    lists to array in test_de

--- tests/test_de.py
+++ tests/test_de.py
@@ -1,31 +1,29 @@
-# -*- coding: utf-8 -*-
+"""Testing de module."""
 
 import pytest
 import numpy as np
-
 import de
 
-
 def test_de_for_equal_arrays():
-    sig = de.calc_de(obs=[1, 2, 3], sim=[1, 2, 3])
+    sig = de.calc_de(obs=np.array([1, 2, 3]), sim=np.array([1, 2, 3]))
     assert sig == 1
 
 def test_de_simulation_equals_obs_mean():
-    sig = de.calc_de(obs=[1, 2, 3], sim=[2, 2, 2])
+    sig = de.calc_de(obs=np.array([1, 2, 3]), sim=np.array([2, 2, 2]))
     assert sig < -1
 
 def test_nse_for_equal_arrays():
-    sig = de.calc_nse(obs=[1, 2, 3], sim=[1, 2, 3])
+    sig = de.calc_nse(obs=np.array([1, 2, 3]), sim=np.array([1, 2, 3]))
     assert sig == 1
 
 def test_nse_simulation_equals_obs_mean():
-    sig = de.calc_nse(obs=[1, 2, 3], sim=[2, 2, 2])
+    sig = de.calc_nse(obs=np.array([1, 2, 3]), sim=np.array([2, 2, 2]))
     assert sig == 0
 
 def test_kge_for_equal_arrays():
-    sig = de.calc_kge(obs=[1, 2, 3], sim=[1, 2, 3])
+    sig = de.calc_kge(obs=np.array([1, 2, 3]), sim=np.array([1, 2, 3]))
     assert sig == 1
 
 def test_kge_simulation_equals_obs_mean():
-    sig = de.calc_kge(obs=[1, 2, 3], sim=[2, 2, 2])
+    sig = de.calc_kge(obs=np.array([1, 2, 3]), sim=np.array([2, 2, 2]))
     assert sig < 0
