9735ac4d74a83436d325a903bd5df566e25bd865
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2020-03-18 15:19:30 +0100

    added pytest.approx to assert to avoid OS dependent rounding errors.

--- tests/test_nse.py
+++ tests/test_nse.py
@@ -8,27 +8,27 @@ from de import nse
 def test_nse_for_arrays():
     eff = nse.calc_nse(obs=np.array([1.5, 1, 0.8, 0.85, 1.5, 2]),
                        sim=np.array([1.6, 1.3, 1, 0.8, 1.2, 2.5]))
-    assert eff == 0.56482525366403
+    assert eff == pytest.approx(0.5648252536640361)
 
 def test_nse_dec_for_arrays():
     eff = nse.calc_nse_dec(obs=np.array([1.5, 1, 0.8, 0.85, 1.5, 2]),
                            sim=np.array([1.6, 1.3, 1, 0.8, 1.2, 2.5]))
-    assert eff == 0.92510767049232
+    assert eff == pytest.approx(0.9251076704923259)
 
 def test_nse_beta_for_arrays():
     beta = nse.calc_nse_beta(obs=np.array([1.5, 1, 0.8, 0.85, 1.5, 2]),
                              sim=np.array([1.6, 1.3, 1, 0.8, 1.2, 2.5]))
-    assert beta == 0.29078287207506
+    assert beta == pytest.approx(0.2907828720750609)
 
 def test_nse_alpha_for_arrays():
     alpha = nse.calc_nse_alpha(obs=np.array([1.5, 1, 0.8, 0.85, 1.5, 2]),
                                sim=np.array([1.6, 1.3, 1, 0.8, 1.2, 2.5]))
-    assert alpha == 1.28120574551669
+    assert alpha == pytest.approx(1.2812057455166919)
 
 def test_nse_r_for_arrays():
     lin_cor = nse.calc_nse_r(obs=np.array([1.5, 1, 0.8, 0.85, 1.5, 2]),
                              sim=np.array([1.6, 1.3, 1, 0.8, 1.2, 2.5]))
-    assert lin_cor == 0.89402818505835
+    assert lin_cor == pytest.approx(0.8940281850583509)
 
 def test_nse_for_equal_arrays():
     sig = nse.calc_nse(obs=np.array([1, 2, 3, 4, 5]),
