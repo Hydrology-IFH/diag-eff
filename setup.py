14109d8f731f6f1b425ee700b82f1b269a118d6a
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2020-03-20 16:39:32 +0100

    modified classifiers in setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     author_email="robin.schwemmle@hydrology.uni-freiburg.de",
     license="GPLv3",
     classifiers=[
-        "Development Status :: 1 - Beta",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Modelling",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
@@ -48,7 +47,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.8"
     ],
     python_requires=">=3.6",
     packages=find_packages(exclude=["docs"]),
