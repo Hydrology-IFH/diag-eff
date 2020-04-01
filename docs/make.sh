9ae3483f0ec36a705af87c31860422956c4efec5
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2020-04-01 11:57:09 +0200

    updated make.sh in docs.

--- docs/make.sh
+++ docs/make.sh
@@ -3,6 +3,9 @@
 # set path to project folder
 cd /Users/robinschwemmle/Desktop/PhD/diagnostic_efficiency/pkg/docs
 
+# sphinx-build -b html /Users/robinschwemmle/Desktop/PhD/diagnostic_efficiency/pkg/docs /Users/robinschwemmle/Desktop/PhD/diagnostic_efficiency/pkg/docs/_build
+
 # make docs
 make html
+# make latexpdf
 # make man
