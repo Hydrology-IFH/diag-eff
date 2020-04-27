d6f2a27e0011554048a6e80be728852242f4daab
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2020-04-27 17:09:36 +0200

    prepared repo to make it public.

--- ship_to_conda.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# set path to project folder
-cd /Users/robinschwemmle/Desktop/PhD/diagnostic_efficiency/diag-eff
-
-# upload to anaconda
-anaconda upload dist/*.tar.gz
-
-# package is now available at: https://anaconda.org/schwemro/diag-eff
